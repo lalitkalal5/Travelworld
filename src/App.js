f68ba36a764d6a04b53c6ddae5509925956b5f2d
Lalit kalal <lalitkalal171@gmail.com>
2024-11-12 20:23:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import {  BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact';
+import LICPage from './components/Lic';
 function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
     <Route path='/' Component={Home} />  
+    <Route path='/Lic' Component={LICPage} />  
     <Route path='/about' Component={About} />  
-    <Route path='/Contact' Component={Contact} />  
+    <Route path='/Contact' Component={Contact} /> 
     </Routes>
     
     </BrowserRouter>
-    </>
+    </> 
   );
 }
 
